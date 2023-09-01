eaa68a04406a8941894d27811677a1674cf59e50
aydun1 <ayduno@gmail.com>
2023-09-01 13:17:46 +1000

    Fix extra labels

--- make-racking-labels.js
+++ make-racking-labels.js
@@ -9,18 +9,18 @@ const document = new DOMImplementation().createDocument('http://www.w3.org/1999/
 
 // vvv configuration options vvv
 const isles = [
-  {isle: 'A', levels: [1, 2], sections: 24, arrows: 'l'},
-  {isle: 'B', levels: [1, 2, 3], sections: 21, arrows: 'r'},
-  {isle: 'C', levels: [1, 2, 3], sections: 21, arrows: 'l'},
-  {isle: 'D', levels: [1, 2, 3], sections: 28, arrows: 'r'},
-  {isle: 'E', levels: [1, 2, 3, 4], sections: 32, sides: 2},
-  {isle: 'F', levels: [1, 2, 3, 4], sections: 32, sides: 2},
-  {isle: 'G', levels: [1, 2, 3, 4], sections: 32, sides: 2},
+  {isle: 'A', levels: [1, 2], sections: 22, arrows: 'l'},
+  {isle: 'B', levels: [1, 2, 3], sections: 19, arrows: 'r'},
+  {isle: 'C', levels: [1, 2, 3], sections: 19, arrows: 'l'},
+  {isle: 'D', levels: [1, 2, 3], sections: 26, arrows: 'r'},
+  {isle: 'E', levels: [1, 2, 3, 4], sections: 62, sides: 2},
+  {isle: 'F', levels: [1, 2, 3, 4], sections: 62, sides: 2},
+  {isle: 'G', levels: [1, 2, 3, 4], sections: 62, sides: 2},
   {isle: 'H', levels: [1, 2, 3, 4], sections: 76, arrows: 'rllrrllrrl-rllrrllrrllrrllrrllrrllrr-llrrl-rllrr-llrrl-rllrr-llrrl-rllrr-llr'}
 ];
 const outputPdfFileName = 'out/barcodes.pdf';
 const outputBinsFileName = 'out/bins.txt';
-const colours = ['acb7b8', 'f15921', '9ad2ae', '15c0f2', 'b18ec1', 'fff101'];
+const colours = ['b18ec1', 'fff101', 'acb7b8', 'f15921', '9ad2ae', '15c0f2'];
 const pageWidth = 600;
 const pageHeight = 450;
 const headingRatio = 0.08;
@@ -80,20 +80,22 @@ function initPageNode() {
 }
 
 function addLabelToPage(labelNode, labelHeight, labelWidth) {
+  const horizontalSpacing = 0;
   const fitX = Math.floor(pageWidth / labelWidth);
-  const extrasX = Math.floor((pageHeight - labelHeight) / labelWidth);
-  const extrasY = Math.floor(pageWidth / labelHeight);
-  const extraLabels = extrasX * extrasY;
+  const extrasY = Math.floor((pageHeight - labelHeight) / labelWidth);
+  const extrasX = Math.floor(pageWidth / (labelHeight + horizontalSpacing));
+  const extraLabels = extrasY * extrasX;
   const totalLabels = fitX + extraLabels;
   if (pageLabelNumber < fitX) {
     labelNode.setAttribute('transform', `translate(${leftMargin + pageLabelNumber * labelWidth}, ${pageHeight - labelHeight})`);
     pageNode.appendChild(labelNode);
   } else if (pageLabelNumber < totalLabels && !onlyVertical) {
     const extra = extraLabels - (totalLabels - pageLabelNumber);
-    const extra2 = extraLabels - (totalLabels - pageLabelNumber);
-    const moveX = labelWidth * (extra % extrasX) + labelWidth;
-    const moveY = labelHeight * (Math.floor(extra2 /extrasY))
-    labelNode.setAttribute('transform', `rotate(270) translate(-${moveX}, ${moveY})`);
+    const xCoord = extra % extrasX;
+    const yCoord = Math.floor(extra / extrasX);
+    const moveX = labelHeight * xCoord + horizontalSpacing * xCoord;
+    const moveY = labelWidth * yCoord + labelWidth;
+    labelNode.setAttribute('transform', `rotate(270) translate(-${moveY}, ${moveX})`);
     pageNode.appendChild(labelNode);
   } else {
     saveToPdf();
